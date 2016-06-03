96edf0c1bcd6227e5327bc881bed5b384a00284e
Alex Richardson <alex@rangle.io>
2016-06-02 20:39:58 -0400

    server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('npm heatmap');
+var express = require('express')
+var logger = require('morgan')
+var app = express();
+
+app.use(logger('dev'))
+app.use(express.static(__dirname + '/src'))
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+});
